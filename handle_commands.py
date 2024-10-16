b1f5fe5cb8c652d483c719e33f0b30ea4db386b2
LeftTry <lerest.go@gmail.com>
2024-10-16 15:31:06 +0600

    [Minor] Restructure adn clean up

---
+++ handle_commands.py
@@ -0,0 +1,28 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import CallbackContext
+
+from check_message import check_message
+
+admin_chat_id = -1
+
+async def handle_group_message(update: Update, context: CallbackContext):
+    result = check_message(update, context)
+    if not result:
+        spam_msg_id = update.message.id
+        spam_user = update.message.from_user
+        group = update.effective_chat
+        await context.bot.delete_message(group.id, spam_msg_id)
+        await context.bot.send_message(
+            chat_id=admin_chat_id,
+            text=f"Message {spam_msg_id} in group {group.effective_name}:  was deleted. Sender: {spam_user.username}"
+        )
+
+
+async def start(update: Update, context: CallbackContext):
+    keyboard = [[
+        "/start"
+    ]]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    global admin_chat_id
+    admin_chat_id = update.message.chat_id
+    await context.bot.send_message(chat_id=admin_chat_id, text="Bot set up.", reply_markup=reply_markup)
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import logging
-import os
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
-from telegram import Update, ChatPermissions, ReplyKeyboardMarkup
-from telegram.constants import ChatType
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, CallbackContext, \
-    ConversationHandler
-
-from check_message import check_message
+from handle_commands import start, handle_group_message
 
 TOKEN_FILE = "token.txt"
-START = range(3)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -25,29 +19,6 @@ def load_token():
         print("Token file not found.")
         return None
 
-admin_chat_id = -1
-
-async def handle_group_message(update: Update, context: CallbackContext):
-    result = check_message(update, context)
-    if not result:
-        spam_msg_id = update.message.id
-        spam_user = update.message.from_user
-        group = update.effective_chat
-        await context.bot.delete_message(group.id, spam_msg_id)
-        await context.bot.send_message(
-            chat_id=admin_chat_id,
-            text=f"Message {spam_msg_id} in group {group.effective_name}:  was deleted. Sender: {spam_user.username}"
-        )
-
-
-async def start(update: Update, context: CallbackContext):
-    keyboard = [[
-        "/start"
-    ]]
-    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-    global admin_chat_id
-    admin_chat_id = update.message.chat_id
-    await context.bot.send_message(chat_id=admin_chat_id, text="Bot set up.", reply_markup=reply_markup)
 
 def main():
     token = load_token()
@@ -55,6 +26,7 @@ def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & filters.ChatType.SUPERGROUP, handle_group_message))
     application.run_polling()
+
 
 if __name__ == "__main__":
     main()
